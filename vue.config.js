e06d9e3a42adce6d8da626e47ca3603f01f0ffd7
zhangce <z874010534@vip.qq.com>
2022-01-09 16:21:08 +0800

    12231

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,4 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/githubAction' : '/',
   productionSourceMap: false,
-  devServer: {
-    disableHostCheck: true,
-    proxy: {
-      '/mock': {
-        target: 'https://staging.qq.com',
-        // target: 'https://api.qq.com',
-        pathRewrite: {
-          '/mock': ''
-        }
-      },
-    }
-  },
 };
